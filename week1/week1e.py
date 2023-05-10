5b5784fe1716664256c4891cf76bdaa36f6144bb
emreyazicicode <133089486+emreyazicicode@users.noreply.github.com>
2023-05-10 13:11:19 +0400

    Create week1e.py

---
+++ week1/week1e.py
@@ -0,0 +1,6 @@
+talebeler = [
+	{'name': 'ahmet', 'no':12342, 'courses': ['data science', 'analytics', 'statistics', 'maths']},
+	{'name': 'ali', 'no':1389, 'courses': ['statistics']},
+	{'name': 'mehmet', 'no':389, 'courses': ['maths', 'analytics']},
+]
+
