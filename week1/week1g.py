56e922b311773f774c4ff188711eb1a0d6b162cf
emreyazicicode <133089486+emreyazicicode@users.noreply.github.com>
2023-05-11 12:40:48 +0400

    Add files via upload

---
+++ week1/week1g.py
@@ -0,0 +1,16 @@
+students = {
+    'Minəxanım Hacımuradova':[0, 7, 5, 6, 4, 4],
+    'Lala Taghiyeva':[2, 5, 3, 7, 1, 3],
+    'Yusif Ağasalamlı':[3, 4, 2, 3, 2, 2],
+    'Həbibə Məmmədli':[3, 6, 2, 3, 2, 7],
+    'Niyyət Rzayev':[3, 6, 4, 5, 6, 4],
+    'Adil Rəhimov':[4, 5, 4, 3, 2, 5],
+    'Riyad Əhmədov':[4, 5, 5, 4, 4, 6],
+    'Ləman Rəhimli':[4, 6, 3, 5, 4, 4],
+    'Riyad Əbdürəhimov':[4, 6, 4, 5, 7, 6],
+    'İlyas Abbasov':[4, 6, 5, 6, 3, 3],
+    'Lalə Məmmədli':[4, 8, 6, 6, 4, 2],
+    'Anar Əhmədov':[5, 7, 5, 6, 6, 3],
+    'Mahmizər Həsənova':[6, 4, 3, 7, 5, 4],
+    'Mədinə Abdulsəmədova':[6, 7, 5, 4, 6, 3]
+}
