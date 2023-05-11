7bdf7cae96865a62379870a3a2f9097b79b67a97
emreyazicicode <133089486+emreyazicicode@users.noreply.github.com>
2023-05-11 13:51:09 +0400

    Update week1g.py

--- week1/week1g.py
+++ week1/week1g.py
@@ -14,3 +14,34 @@ students = {
     'Mahmizər Həsənova':[6, 4, 3, 7, 5, 4],
     'Mədinə Abdulsəmədova':[6, 7, 5, 4, 6, 3]
 }
+
+
+def ikitalebekiyasla( t1, t2 ):
+    tv1 = students[t1]
+    tv2 = students[t2]
+
+    fark = 0
+    for i in range(6):
+        fark += abs(tv1[i] - tv2[i])
+
+    return fark
+
+
+
+en_benzer = 100000
+en_benzer_nefer = None
+
+for s in students:
+    result = ikitalebekiyasla( s, 'Lalə Məmmədli' )
+    if s != 'Lalə Məmmədli':
+        if result < en_benzer:
+            print("BULUNDU:", s, "result=", result, "en_benzer=", en_benzer, "en_benzer_nefer", en_benzer_nefer  )
+            en_benzer = result
+            en_benzer_nefer = s
+        else:
+            print("BENZER DEGIL:", s, "result=", result, "en_benzer=", en_benzer, "en_benzer_nefer", en_benzer_nefer  )
+
+
+
+
+
