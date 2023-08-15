b3da52e576898591cb5ba76c3e2187091a6687be
emreyazicicode <133089486+emreyazicicode@users.noreply.github.com>
2023-08-15 10:52:20 +0300

    Add files via upload

---
+++ week14/week14_2_example.py
@@ -0,0 +1,54 @@
+
+#: Imports
+import numpy as np
+import math
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.cluster import KMeans
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
+
+#: Load dataset
+df = pd.read_excel("week14_satisfaction.xlsx")
+
+#: Feature mapping
+df['satisfaction_v2'] = df['satisfaction_v2'].map({'satisfied': 1, 'neutral or dissatisfied': 0})
+df['Gender'] = df['Gender'].map({'Male': 1, 'Female': 0})
+df['Customer Type'] = df['Customer Type'].map({'Loyal Customer': 1, 'disloyal Customer': 0})
+df['Type of Travel'] = df['Type of Travel'].map({'Personal Travel': 1, 'Business travel': 0})
+df['Class'] = df['Class'].map({'Eco': 0, 'Eco Plus': 0, 'Business': 1})
+
+#: Sort values
+df = df.sort_values(by = ['id'])
+
+#: Remove unnecessary
+del df['id']
+
+#: Discarding Outliers, : CLIPPING, REMOVING
+
+#: Filling empties
+
+#: Normalize
+
+#: Transform
+
+#: Feature mining
+
+#: Feature elimination
+
+#: Data aging
+
+#: Re-balancing
+
+#: Shuffling
+
+#: Problem representation, splitting etc
+
+#: Modelling
+
+#: Testing
+
+#: Error modeling
+
+#: Saving to pickle, saving the constants
